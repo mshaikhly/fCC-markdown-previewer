3bd66712d3607bbc9e0481bc48760f1ccf26e73c
Mohammed <moh.alshaikhly@gmail.com>
2024-06-02 01:44:54 +0100

    third commit

--- src/components/MarkdownPreview.tsx
+++ src/components/MarkdownPreview.tsx
@@ -1,21 +1,18 @@
 import { useSelector } from 'react-redux';
 import { RootState } from '../store/store';
-import ReactMarkdown from 'react-markdown';
-import Card from 'react-bootstrap/Card';
+import { marked } from 'marked';
 
 const MarkdownPreview = () => {
   const markdownText = useSelector((state: RootState) => state.markdown.markdownText);
   const isDarkMode = useSelector((state: RootState) => state.theme.isDarkMode);
 
+  const getMarkdownText = () => {
+    const rawMarkup = marked(markdownText);
+    return { __html: rawMarkup };
+  };
+
   return (
-    <>
-      <h1>Preview</h1>
-      <Card className={`mb-3 ${isDarkMode ? 'dark-mode' : ''}`}>
-        <Card.Body>
-          <ReactMarkdown>{markdownText}</ReactMarkdown>
-        </Card.Body>
-      </Card>
-    </>
+    <div id="preview" className={`${isDarkMode ? 'dark-mode' : ''}`} dangerouslySetInnerHTML={getMarkdownText()} />
   );
 };
 
